e8361aee3e82f52d783a04dbf6261014c422b644
conde <igui96@gmail.com>
2019-10-17 16:17:07 +0200

    p2 init

---
+++ Practica2/pr2.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let listaTareas = [
+    { text: "Preparar práctica AW", tags: ["AW", "practica"] },
+    { text: "Mirar fechas congreso", done: true, tags: [] },
+    { text: "Ir al supermercado", tags: ["personal"] },
+    { text: "Mudanza", done: false, tags: ["personal"] },
+    ];
+    
